274db3b64729513fbcd7c3504b27e4adcdce4ffb
mlmar <marcloussm@gmail.com>
2021-10-09 22:30:47 -0700

    .

--- globalgiving-react/src/js/util/System.js
+++ globalgiving-react/src/js/util/System.js
@@ -1,6 +1,4 @@
 const DEV = process.env.REACT_APP_DEV;
-const HOME_URL = process.env.REACT_APP_HOME_URL;
-const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-export const HOME_URL = DEV ? 'http://localhost:3000' : HOME_URL;
-export const SERVER_URL = DEV ? 'http://localhost:3300' : HOME_URL;
+export const HOME_URL = DEV ? 'http://localhost:3000' : process.env.REACT_APP_HOME_URL;
+export const SERVER_URL = DEV ? 'http://localhost:3300' : process.env.REACT_APP_SERVER_URL;
